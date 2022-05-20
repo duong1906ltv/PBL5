fd20902aed4f3cba504ca6d7ee29a3ca232d1c2c
ducchinhbg01 <ducchinhbg01@gmail.com>
2022-05-20 13:22:58 +0700

    fix form

--- client/src/pages/Forgot.jsx
+++ client/src/pages/Forgot.jsx
@@ -1,19 +1,22 @@
 import Wrapper from '../assets/wrappers/Forgot'
-
+import { Logo, FormRow, Alert } from "../components";
 const Forgot = () => {
     return(
-        <Wrapper>
-           
-            <div class="login">
+        <Wrapper className="full-page">
+         <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css " />
+             <div class="login">
+                    <Logo/>
                     <h2>Can not Login?</h2>
                     <input type="text" placeholder='Fill your Email'/>
                     
                     <button>Send recovery link</button>
                     <div class="signup">
-                        <a href="">Back to Login</a>
+                        <a href="http://localhost:3000/Login">Back to Login</a>
                     </div>
 
-            </div> 
+             </div> 
+         
+            
            
                
            
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,18 @@
 import Wrapper from '../assets/wrappers/Signup'
-
+import { Logo, FormRow, Alert } from "../components";
 const Signup = () => {
     return(
         <Wrapper >
             <div class="login">
-                <h1>Register</h1>
+                <Logo />
+                <h1>Signup</h1>
+                
                 <input type="text" placeholder="Name" />
                 <input type="text" placeholder="Email"/>
                 <input type="password" placeholder="Password"/>
                 <button>Submit</button>
                 <div class="signup">
-                    Already a member? <a href="">Login</a>
+                    Already a member? <a href="http://localhost:3000/Login">Login</a>
                 </div>
                 
             </div>   
